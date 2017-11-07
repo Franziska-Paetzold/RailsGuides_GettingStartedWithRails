7245b5f45fcd8935cfa76d565c6a7b72e5b7306c
Franziska Pätzold <s0554620@htw-berlin.de>
2017-11-07 16:31:35 +0100

    added comments and authentification

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,11 @@
 class WelcomeController < ApplicationController
+
+  http_basic_authenticate_with name: "Babsi", password: "BabsiNenntSichImmerFranzi", only: :destroy
+
   def index
+  end
+
+  def create
+    @article = Article.find(params[:article_id])
   end
 end
